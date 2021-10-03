479217200f0103cc8bdd167512cdc0050298fc6a
Niels de Jong <n.a.de.jong@student.rug.nl>
2021-10-03 16:05:26 +0200

    Start fresh from a separate branch

--- constants.py
+++
@@ -1,5 +0,0 @@
-GOOGLE_CLOUD_KEY_FILE: str = 'google-cloud-key.json'
-ORIGINAL_DATA_FILE: str = 'original.json'
-ORIGINAL_TRANSLATIONS_FILE: str = 'translations.csv'  # still contains Freebase MIDs
-MODIFIED_TRANSLATIONS_FILE: str = 'modified-translations.csv'  # all entries contain proper names for entities
-SPLITS_LOCATION: str = 'splits'
