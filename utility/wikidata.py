ea7f8d20575b4d20072a411a8505bbf2039fdb7a
Niels de Jong <n.a.de.jong@student.rug.nl>
2021-10-12 22:01:15 +0200

    Further add to the addenda
    We're at around 10K QA pairs now.

--- utility/wikidata.py
+++ utility/wikidata.py
@@ -14,16 +14,28 @@ from utility.typing import HTTPAddress, WikiDataSymbol
 
 WIKIDATA_URL = HTTPAddress('https://query.wikidata.org/sparql')
 WIKIDATA_QUERY_WAIT = float(60 // 21)
-WIKIDATA_ERROR_WAIT = 5
+WIKIDATA_TOO_MANY_REQUESTS_WAIT = 5
+CONNECTION_DROPPED_WAIT = 30
 
 
 def wikidata_query(query: str, variables: Tuple[str, ...], wait: bool = False) -> Dict[str, Tuple[str, ...]]:
-	req = rq.get(WIKIDATA_URL, params={'format': 'json', 'query': query})
+	connect_succeeded: bool = False
+	while not connect_succeeded:
+		try:
+			req = rq.get(WIKIDATA_URL, params={'format': 'json', 'query': query})
+			connect_succeeded = True
+		except rq.exceptions.ConnectionError:
+			print(
+				'[query_wikidata] Encountered a connection problem. Retrying in %d seconds...' %
+				(CONNECTION_DROPPED_WAIT,))
+			time.sleep(CONNECTION_DROPPED_WAIT)
 	if not req.ok and req.status_code != 429:
 		raise RuntimeError('[query_wikidata] Encountered a problem!\nQuery:\n%s\nCode: %d.' % (query, req.status_code))
 	while req.status_code == 429:
-		print('[query_wikidata] Encountered a \'429 - Too Many Requests\' error. Retrying in 5 seconds...')
-		time.sleep(WIKIDATA_ERROR_WAIT)
+		print(
+			'[query_wikidata] Encountered a \'429 - Too Many Requests\' error. Retrying in %d seconds...' %
+			(WIKIDATA_TOO_MANY_REQUESTS_WAIT,))
+		time.sleep(WIKIDATA_TOO_MANY_REQUESTS_WAIT)
 		req = rq.get(WIKIDATA_URL, params={'format': 'json', 'query': query})
 	d: Dict[str, Tuple[str, ...]] = {variable: tuple() for variable in variables}
 	for bind in req.json()['results']['bindings']:
