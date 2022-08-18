0e41d4342db2b2f17b269e878964698c0b3d0942
Niels de Jong <n.a.de.jong@student.rug.nl>
2022-08-18 23:56:30 +0200

    Reformat Python DS creation folder, add one more
    The new folder is for the language model.

---
+++ source/py-model/dutch_kbqa_py_model/main.py
@@ -0,0 +1,6 @@
+"""Symbols for running the main program from the command-line."""
+
+
+if __name__ == '__main__':
+    pass
+
--- source/py-model/setup.py
+++ source/py-model/setup.py
@@ -1,15 +1,15 @@
-"""Sets up the `py-ds-create` Python sub-project."""
+"""Sets up the `py-model` Python sub-project."""
 
 from setuptools import setup, find_packages
 
 
 if __name__ == '__main__':
-    setup(name='dutch-kbqa-py-ds-create',
+    setup(name='dutch-kbqa-py-model',
           version='0.1.0',
           author='Niels de Jong',
           author_email='n.a.de.jong@student.rug.nl',
-          description='Symbols for creating a Dutch derivation of the ' +
-                      'LC-QuAD 2.0 dataset.',
+          description='Symbols for fine-tuning a BERT-based transformer ' +
+                      'for Dutch WikiData question-answering.',
           url='https://github.com/some-coder/dutch-kbqa',
           packages=find_packages())
 	
